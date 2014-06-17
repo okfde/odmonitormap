726df95c91514f4d0b4e3cbcd5fdcbe2f94c5460
Matt Fullerton <matt.fullerton@gmail.com>
2014-06-17 09:44:35 +0200

    Calculate and show number of cities and datasets

--- odmonitormap.js
+++ odmonitormap.js
@@ -1,5 +1,3 @@
-// $(function(){
-  /* globals d3, $, _, L, console */
   'use strict';
   var cityURL = 'data/index.csv';
 
@@ -82,12 +80,22 @@
     html.push('</ul>');
     return html.join('');
   };
+  
+  //How many cities
+  var cityCount = 0;
+  //How many data sets in total
+  var completeCount = 0;
 
   var getCityContent = function(city, marker, map) {
     //Get the city content, and if it exists, add it to the map
     var cityslug = city.kurzname;
     if (cityslug != "") {
+      //Attempt to load the city data and update page data when done
       loadCity(cityslug, function(data){
+        //The final decider of whether to show a marker is whether the data file exists,
+        //but we could also do it based on GID in the index and prevent execution of this
+        //code. Prevention currently occurs earlier if there is no short name, but at some
+        //point we might enter them all regardless of whether we have data
         if (data != null) {
           var count = 0;
           _.each(data, function(d){
@@ -95,6 +103,8 @@
               count += 1;
             }
           });
+          completeCount += count;
+          
           var emailContent = "";
           if (city['Kontakt Mail'] !== undefined) emailContent = "<li>Kontakt: <a href=\"mailto:"+city['Kontakt Mail']+"\">"+city['Kontakt Mail']+"</a></li>";
           marker.bindPopup('<h2>' + city.Stadtname + '</h2><ul><li><a href=\"' + city.DOMAIN + '\">' + city.DOMAIN + '</a></li><li>' + count + ' Datensätze' + emailContent + '</ul>', {
@@ -102,7 +112,13 @@
           }).on('popupopen', function(){
             $('#infobox').html(showCity(city, data, count));
           });
+          
           marker.addTo(map);
+          cityCount++;
+          
+          //Update the page
+          $('#numCities').text(cityCount);
+          $('#numDatasets').text(completeCount);
         }
       });
     }
@@ -129,4 +145,3 @@
     });
 
   });
-// });
